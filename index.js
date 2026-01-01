2a2658e8427014a53be0286d36453453df23505b
Thetoan341 <thetoanq@gmail.com>
2026-01-01 21:08:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const login = require("fca-unofficial");
 
+login(
+  {
+    email: "EMAIL_FACEBOOK",
+    password: "MAT_KHAU_FACEBOOK"
+  },
+  (err, api) => {
+    if (err) return console.error(err);
+
+    console.log("Bot Messenger đã chạy!");
+
+    api.listenMqtt((err, event) => {
+      if (err) return console.error(err);
+
+      if (event.type === "message") {
+        if (event.body === "ping") {
+          api.sendMessage("pong 🏓", event.threadID);
+        }
+      }
+    });
+  }
+);
